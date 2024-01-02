b6fb8a5fd7a17ebee1ac0627155a6905ba0aaae7
Darksller <darksller.sss@gmail.com>
2024-01-02 23:45:57 +0300

    and another one...

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import { Server } from 'socket.io'
-import { express } from 'express'
+import express from 'express'
 import { createServer } from 'http'
 
 const app = express()
-const server = createServer(app)
+const httpServer = createServer(app)
 
-const io = new Server(server, {
+const io = new Server(httpServer, {
 	cors: {
 		origin: '*',
 	},
@@ -13,63 +13,62 @@ const io = new Server(server, {
 
 const rooms = []
 
-io.on('connection', socket => {
-	socket.on('getRooms', callback => {
-		callback({ rooms, status: true })
+httpServer.listen(() => {
+	console.log('listening')
+	io.on('connection', socket => {
+		socket.on('getRooms', callback => {
+			callback({ rooms, status: true })
+		})
+
+		socket.on('addNewRoom', callback => {
+			const newRoomId =
+				rooms.length > 0 ? Math.max(...rooms.map(room => room.id)) + 1 : 1
+			const newRoom = { id: newRoomId.toString(), image: '' }
+			rooms.push(newRoom)
+
+			callback({ ...newRoom, status: true })
+			socket.broadcast.emit('rooms', rooms)
+		})
+
+		socket.on('joinRoom', (roomId, callback) => {
+			const room = rooms.find(room => room.id == roomId)
+			if (!room) return callback({ status: false })
+
+			rooms.map(room => socket.leave(room.id))
+			socket.join(roomId)
+
+			callback({ ...room, status: true })
+		})
+
+		socket.on('getImage', callback => {
+			const roomId = Array.from(socket.rooms)[1]
+			const room = rooms.find(room => room.id == roomId)
+			if (!room) return callback({ status: false })
+
+			callback({ image: room.image, status: true })
+		})
+
+		socket.on('saveImage', (image, callback) => {
+			const roomId = Array.from(socket.rooms)[1]
+			const room = rooms.find(room => room.id == roomId)
+			if (!room) return callback({ status: false })
+
+			room.image = image
+		})
+
+		socket.on('draw', (currentPoint, prevPoint, color, thickness, tool) => {
+			const roomId = Array.from(socket.rooms)[1]
+			socket
+				.to(roomId)
+				.emit('draw', currentPoint, prevPoint, color, thickness, tool)
+		})
+
+		socket.on('clearScreen', callback => {
+			const roomId = Array.from(socket.rooms)[1]
+			const room = rooms.find(room => room.id == roomId)
+			if (!room) return callback({ status: false })
+			room.image = ''
+			socket.to(roomId).emit('clearImage', { image: room.image, status: true })
+		})
 	})
-
-	socket.on('addNewRoom', callback => {
-		const newRoomId =
-			rooms.length > 0 ? Math.max(...rooms.map(room => room.id)) + 1 : 1
-		const newRoom = { id: newRoomId.toString(), image: '' }
-		rooms.push(newRoom)
-
-		callback({ ...newRoom, status: true })
-		socket.broadcast.emit('rooms', rooms)
-	})
-
-	socket.on('joinRoom', (roomId, callback) => {
-		const room = rooms.find(room => room.id == roomId)
-		if (!room) return callback({ status: false })
-
-		rooms.map(room => socket.leave(room.id))
-		socket.join(roomId)
-
-		callback({ ...room, status: true })
-	})
-
-	socket.on('getImage', callback => {
-		const roomId = Array.from(socket.rooms)[1]
-		const room = rooms.find(room => room.id == roomId)
-		if (!room) return callback({ status: false })
-
-		callback({ image: room.image, status: true })
-	})
-
-	socket.on('saveImage', (image, callback) => {
-		const roomId = Array.from(socket.rooms)[1]
-		const room = rooms.find(room => room.id == roomId)
-		if (!room) return callback({ status: false })
-
-		room.image = image
-	})
-
-	socket.on('draw', (currentPoint, prevPoint, color, thickness, tool) => {
-		const roomId = Array.from(socket.rooms)[1]
-		socket
-			.to(roomId)
-			.emit('draw', currentPoint, prevPoint, color, thickness, tool)
-	})
-
-	socket.on('clearScreen', callback => {
-		const roomId = Array.from(socket.rooms)[1]
-		const room = rooms.find(room => room.id == roomId)
-		if (!room) return callback({ status: false })
-		room.image = ''
-		socket.to(roomId).emit('clearImage', { image: room.image, status: true })
-	})
-})
-
-server.listen(8080, function () {
-	console.log('Listening on http://0.0.0.0:8080')
 })
