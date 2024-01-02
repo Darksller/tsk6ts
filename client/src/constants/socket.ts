b4c90a9c8d218341a6f757767e040161502a5846
Darksller <darksller.sss@gmail.com>
2024-01-03 01:39:56 +0300

    1

--- client/src/constants/socket.ts
+++ client/src/constants/socket.ts
@@ -1,3 +1,4 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('localhost:5000')
+//export const socket = io('localhost:5000')
+export const socket = io('https://tsk6-server.vercel.app/')
